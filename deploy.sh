a6b8d7e7f7b77e91a736aa2eec62032fc1b5770e
paul <paul@nms.com.au>
2020-09-02 09:33:02 +1000

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 APIKEY=$1
-if [ "$APIKEY" == "" ]
-then
-  echo "****************************"
-  echo "* APIKEY ***must*** be set *"
-  echo "*                          *"
-  echo "****************************"
-  echo ""
-  echo "Exiting program."
-  exit 1
-fi
+# if [ "$APIKEY" == "" ]
+# then
+#   echo "****************************"
+#   echo "* APIKEY ***must*** be set *"
+#   echo "*                          *"
+#   echo "****************************"
+#   echo ""
+#   echo "Exiting program."
+#   exit 1
+# fi
 echo "Deploying with APIKEY set"
-ansible-playbook -u root -e APIKEY=$APIKEY -i ansible/hosts ansible/wooliesx.yml
+ansible-playbook -u root -e APIKEY=$APIKEY -i hosts wooliesx.yml
